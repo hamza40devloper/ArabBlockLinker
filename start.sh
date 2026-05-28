4ae06acd986dc31c4d4ac2fc98da88fadd0eb01f
hamza <160620205+hamza40devloper@users.noreply.github.com>
2026-05-28 18:29:58 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Downloading PaperMC..."
+
+curl -L -o paper.jar "https://fill-data.papermc.io/v1/objects/2c2af90d6ef0e823c272e7059873e3b7a24e07674e56e3b8d6c63ebff03cf827/paper-26.1.2-66.jar"
+
+echo "Starting server..."
+
+java -Xms1G -Xmx2G -jar paper.jar nogui
