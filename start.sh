c8066bcee0627cd9469f7d10f8d890d476f65e22
hamza <160620205+hamza40devloper@users.noreply.github.com>
2026-05-28 19:13:33 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "Checking PaperMC..."
 
 if [ ! -f paper.jar ]; then
   echo "Downloading PaperMC..."
-  curl -L -o paper.jar "https://fill-data.papermc.io/v1/objects/2c2af90d6ef0e823c272e7059873e3b7a24e07674e56e3b8d6c63ebff03cf827/paper-26.1.2-66.jar"
+  wget -O paper.jar "https://fill-data.papermc.io/v1/objects/2c2af90d6ef0e823c272e7059873e3b7a24e07674e56e3b8d6c63ebff03cf827/paper-26.1.2-66.jar"
 fi
 
 echo "Starting server..."
